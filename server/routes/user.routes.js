011451c0120c2e3c7e97c982d3e3071d01769e83
Josh Lazar <lazarjosh2@gmail.com>
2024-02-29 14:06:02 -0500

    debugging cookie

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,9 +1,10 @@
 const UserController = require('../controllers/user.controller')
+const { authenticate } = require('../config/jwt.config');
 
 
 module.exports = app => {
     app.post('/api/register', UserController.registerUser)
     app.post('/api/login', UserController.loginUser)
     app.post('/api/logout', UserController.logoutUser)
-    app.get('/api/findUserInfo', UserController.findUserInfo)
+    app.get('/api/findUserInfo', authenticate, UserController.findUserInfo)
 }
