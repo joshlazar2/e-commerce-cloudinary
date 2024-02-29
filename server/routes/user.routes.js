a1ac45cd11b716087137b8b0b8f5e5d96aae1725
Josh Lazar <lazarjosh2@gmail.com>
2024-02-29 15:33:54 -0500

    authentication updates

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -5,6 +5,6 @@ const { authenticate } = require('../config/jwt.config');
 module.exports = app => {
     app.post('/api/register', UserController.registerUser)
     app.post('/api/login', UserController.loginUser)
-    app.post('/api/logout', UserController.logoutUser)
+    app.post('/api/logout', authenticate, UserController.logoutUser)
     app.get('/api/findUserInfo', authenticate, UserController.findUserInfo)
 }
