11e640695b896f18ecaeaf4aa46ac4a037cf614e
Josh Lazar <lazarjosh2@gmail.com>
2024-02-29 14:10:05 -0500

    changed port

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ UserRoutes(app);
 const ProductRoutes = require("./routes/product.routes");
 ProductRoutes(app);
     
-app.listen(8000, () => console.log("The server is all fired up on port 8000"));
+app.listen(10000, () => console.log("The server is all fired up on port 10000"));
 
 
 // To get everything up and running always install express and mongoose (npm install)
